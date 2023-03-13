79a6d35c2daa19fa63c38235793cf62940d6e459
Matilde Aarenstrup Christoffersen <mati1398@edu.easj.dk>
2023-03-13 14:39:16 +0100

    Refactored with error handling

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 Vue.createApp({
-    data(){
-        return {
-            word: null,
-            words: [],
-            result: []
-        }
-    }, methods: {
-        reverseString(word) {
-            return word.split("").reverse().join("");
-        },
-        save(word){
-            this.word = word;
-            this.words.push(word)
-            this.words.push(word.toUpperCase())
-            this.words.push(word.toLowerCase())
-            this.words.push(word.charAt(0).toUpperCase() + word.slice(1))
-            this.words.push(word.slice(0,word.length-1) + word.charAt(word.length-1).toUpperCase())
-            this.words.push(this.reverseString(word))
-
-            this.result = this.words
-            this.words = [];
-        },
-        
-    }
-}).mount ("#app")
+  data() {
+    return {
+      word: "",
+      words: [],
+      error: "",
+    };
+  },
+  methods: {
+    reverseString(word) {
+      return word.split("").reverse().join("");
+    },
+    save(word) {
+      if (word == null || word.trim().length === 0 || word == "") {
+        this.error = "Please enter a word";
+        this.words = [];
+      } else {
+        this.error = "";
+        this.words[0] = word;
+        this.words[1] = word.toUpperCase();
+        this.words[2] = word.toLowerCase();
+        this.words[3] = word[0].toUpperCase() + word.slice(1);
+        this.words[4] =
+          word.slice(0, word.length - 1) +
+          word.charAt(word.length - 1).toUpperCase();
+        this.words[5] = this.reverseString(word);
+      }
+    },
+  },
+}).mount("#app");
